6a41d9c309cdbea5dc913f0d3fcf49864865430c
carpediemmaverick1608 <89202493+carpediemmaverick1608@users.noreply.github.com>
2021-08-30 16:51:23 +0530

    Update file2.sh

--- file2.sh
+++ file2.sh
@@ -3,8 +3,6 @@
 # Program name: "printVar.sh"
 # Linux shell script program to create and print the value of variables.
 
-country="India"
-year=2021
 
 echo "Country name: $country"
 echo "Year: $year"
